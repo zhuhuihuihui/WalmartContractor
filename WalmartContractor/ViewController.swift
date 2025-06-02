b34edb0dcd31ca781ffab9a8b076f5948873a148
Scott Zhu <scott.zhu@yahooinc.com>
2025-06-02 12:13:13 -0700

    Initial Commit

---
+++ WalmartContractor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalmartContractor
+//
+//  Created by Scott Zhu on 6/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
